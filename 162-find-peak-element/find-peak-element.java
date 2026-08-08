3f8fc61cbaa8713539ae0318ed76a73e62da0a50
peropranav-1 <pranavkapoor99@gmail.com>
2026-08-08 23:21:02 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (88.38%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        if (nums.length == 1) {
+        if (nums.length <= 1) {
             return 0;
         }
         if (nums[0] > nums[1]) {
@@ -15,13 +15,18 @@ class Solution {
         int r = nums.length - 2;
 
         while (l <= r) {
-            int mid = (r + l) / 2;
+            int mid = l + (r - l) / 2;
 
             if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
                 return mid;
-            } else if (nums[mid - 1] < nums[mid]) {
+            } else if (nums[mid - 1] < nums[mid]
+                    && nums[mid] < nums[mid + 1]) {
                 l = mid + 1;
-            } else {
+            } else if (nums[mid] < nums[mid - 1] &&
+                    nums[mid] > nums[mid + 1]) {
+                r = mid - 1;
+            } else if (nums[mid] < nums[mid - 1] &&
+                    nums[mid] < nums[mid + 1]) {
                 r = mid - 1;
             }
         }
