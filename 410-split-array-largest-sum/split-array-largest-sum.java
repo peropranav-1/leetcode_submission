c62a28add34dc396e73c08b6b4b1c87514720999
peropranav-1 <pranavkapoor99@gmail.com>
2026-08-09 18:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (51.64%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int maxPages = 0;
+        int startPages = 0;
+        for (int num : nums) {
+            maxPages = maxPages + num;
+        }
+        for (int num : nums) {
+            startPages = Math.max(num, startPages);
+        }
+
+        while (startPages <= maxPages) {
+            int mid = (startPages + maxPages) / 2;
+            int studentCount = getStudentCount(nums, mid);
+            if (studentCount > k) {
+                startPages = mid + 1;
+            } else {
+                maxPages = mid - 1;
+            }
+
+        }
+        return startPages;
+    }
+
+    // 10 to 32
+    private int getStudentCount(int[] nums, int pages) {
+        int pagesPerStudent = 0;
+        int studentCount = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (pagesPerStudent + nums[i] <= pages) {
+                pagesPerStudent = pagesPerStudent + nums[i];
+            } else {
+                pagesPerStudent = nums[i];
+                studentCount++;
+            }
+        }
+        return studentCount;
+    }
+}
