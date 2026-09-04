d3cfa43bf14327a197bb2d093900ea3e02c30c1c
peropranav-1 <pranavkapoor99@gmail.com>
2026-09-05 04:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (86.75%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,35 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        return numberOfWay(n, dp);
-    }
+        // int[] dp = new int[n + 1];
+        // return numberOfWay(n, dp);
 
-    private int numberOfWay(int n, int[] dp) {
-        if (n == 0 || n == 1) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
             return 1;
         }
-        if (dp[n] != 0) {
-            return dp[n];
-        }
-        int resp = numberOfWay(n - 1, dp) + numberOfWay(n - 2, dp);
-        dp[n] = resp;
-        return resp;
+        return numberOfWay(n, 1, 1);
     }
+
+    // private int numberOfWay(int n, int[] dp) {
+    //     if (n == 0 || n == 1) {
+    //         return 1;
+    //     }
+    //     if (dp[n] != 0) {
+    //         return dp[n];
+    //     }
+    //     int resp = numberOfWay(n - 1, dp) + numberOfWay(n - 2, dp);
+    //     dp[n] = resp;
+    //     return resp;
+    // }
+    private int numberOfWay(int n, int prev0, int prev1) {
+        for (int i = 2; i <= n; i++) {
+            int cur = prev0 + prev1;
+            prev0 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+
 }
