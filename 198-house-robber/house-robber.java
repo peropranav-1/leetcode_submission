55468f60ac7fe10a107a0e7f0b5a45ba2472298d
peropranav-1 <pranavkapoor99@gmail.com>
2026-09-12 01:34:11 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (19.33%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,24 +1,43 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        for (int i = 0; i < dp.length; i++) {
-            dp[i] = -1;
-        }
-        return getRob(nums.length - 1, nums, dp);
+        // int[] dp = new int[nums.length];
+        // for (int i = 0; i < dp.length; i++) {
+        //     dp[i] = -1;
+        // }
+        // return getRob(nums.length - 1, nums, dp);
+
+        return getRobTabulation(nums);
     }
 
-    private int getRob(int index, int[] nums, int[] dp) {
-        if (index == 0)
-            return nums[0];
-        if (index < 0)
-            return 0;
-        if (dp[index] != -1)
-            return dp[index];
+    // private int getRob(int index, int[] nums, int[] dp) {
+    //     if (index == 0)
+    //         return nums[0];
+    //     if (index < 0)
+    //         return 0;
+    //     if (dp[index] != -1)
+    //         return dp[index];
 
-        int pick = nums[index] + getRob(index - 2, nums, dp);
-        int nonPick = 0 + getRob(index - 1, nums, dp);
+    //     int pick = nums[index] + getRob(index - 2, nums, dp);
+    //     int nonPick = 0 + getRob(index - 1, nums, dp);
 
-        dp[index] = Math.max(pick, nonPick);
-        return dp[index];
+    //     dp[index] = Math.max(pick, nonPick);
+    //     return dp[index];
+    // }
+
+    private int getRobTabulation(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        for (int index = 1; index < nums.length; index++) {
+            int pick = nums[index];
+
+            if (index > 1)
+                pick = pick + dp[index - 2];
+            int nonPick = 0 + dp[index - 1];
+
+            dp[index] = Math.max(pick, nonPick);
+        }
+
+        return dp[nums.length - 1];
     }
 }
